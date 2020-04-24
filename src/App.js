afa79b5c029922e7c62ea48e0a0c32470200268a
Ian McElroy <mcelroyian@gmail.com>
2020-04-24 11:11:46 -0500

    form created. route working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Route, Switch, Link } from 'react-router-dom'
+import Home from './Home'
+import Form from './Form'
+import "./index.css";
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+      <Link to='/'>
+       <h1>Lambda Eats</h1>
+      </Link>
+      
+      </header>
+      <div className='container'>
+        <Switch>
+          <Route path="/pizza" >
+            <Form />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        
+        
+      </div>
+      
     </>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Form() {
+
+    return (
+        <div className="form-container">
+            <h2>Build your Pizza</h2>
+            <form>
+                <input type="text" name="" />
+                <input type="text" name="" />
+                <input type="text" name="" />
+                <input type="text" name="" />
+                <input type="text" name="" />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
