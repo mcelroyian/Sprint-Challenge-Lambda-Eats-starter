5f5ac7100a3fad3c5cce75b01bb60355d24dbf8c
Ian McElroy <mcelroyian@gmail.com>
2020-04-24 12:12:24 -0500

    form works. submits saves to state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
 import React, { useState } from "react";
 import { Route, Switch, Link } from 'react-router-dom'
+import axios from 'axios'
 import Home from './Home'
 import Form from './Form'
 import "./index.css";
+
+
+
+//Data ENDPOINT
+const url = 'https://reqres.in/api/users'
+
+// USER TEMPLATE
+
+const pizzaDefault = {
+  name: '',
+  size: '',
+  ham: false,
+  pepperoni: false,
+  mushrooms: false,
+  bacon: false,
+  pineapple: false,
+  olives: false,
+  instructions: '',
+}
+
+// ERRORS Default
+const errorsDefault = {
+  name: '',
+}
+
+// YUM Validation
+
+
 const App = () => {
+
+  ///State
+  const [pizzaForm, setPizzaForm] = useState(pizzaDefault)
+  const [isDisabled, setIsDisabled] = useState(false)
+  const [errors, setErrors] = useState(errorsDefault)
+  const [pizzas, setPizzas] = useState([])
+
+
+
+  //Event Handlers
+
+  const inputChange = e => {
+    const name = e.target.name
+    const value = e.target.value
+
+    setPizzaForm({...pizzaForm, [name]:value,})
+  }
+
+  const checkChange = e => {
+    const name = e.target.name
+    const value = e.target.checked
+
+    setPizzaForm({...pizzaForm, [name]:value,})
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    sendPizza(pizzaForm)
+    setPizzaForm(pizzaDefault)
+  }
+
+  //Helpers
+  const sendPizza = (pizzaDetails) => {
+    //post axios call to server
+    axios.post(url, pizzaDetails)
+      .then(res => {
+        debugger
+        setPizzas([...pizzas, res.data])
+      })
+      .catch(err => {
+        debugger
+      })
+    //save response in state
+  }
+
+  //Validate form
+
+
   return (
     <>
       <header>
@@ -15,7 +92,7 @@ const App = () => {
       <div className='container'>
         <Switch>
           <Route path="/pizza" >
-            <Form />
+            <Form disabled={isDisabled} values={pizzaForm} inputChange={inputChange} checkChange={checkChange} onSubmit={onSubmit}  />
           </Route>
           <Route path='/'>
             <Home />
--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,76 @@
 import React from 'react'
 
-function Form() {
-
+function Form(props) {
+    const {disabled, values, inputChange, checkChange, onSubmit} = props
     return (
         <div className="form-container">
             <h2>Build your Pizza</h2>
             <form>
-                <input type="text" name="" />
-                <input type="text" name="" />
-                <input type="text" name="" />
-                <input type="text" name="" />
-                <input type="text" name="" />
-                <button>Submit</button>
+            <label >Name: &nbsp;
+            <input 
+                onChange={inputChange}
+                value={values.name}
+                type="text" 
+                name="name" />
+            </label>
+                <label>Pizza Size: 
+                <select 
+
+                    onChange={inputChange}
+                    value={values.size}
+                    name='size'>
+                    <option defaultValue=''>Select a Size</option>
+                    <option value='S'>Small</option>
+                    <option value='M'>Medium</option>
+                    <option value='L'>Large</option>
+                    <option value='XL'>Extra Large</option>
+                </select>
+                </label>
+                <h4>toppings</h4>
+                <div>
+                <label>Ham
+                <input 
+                    onChange={checkChange}
+                    checked={values.ham}
+                    type="checkbox" 
+                    name="ham" /></label>
+                    <label>Pepperoni
+                <input 
+                    onChange={checkChange}
+                    checked={values.pepperoni}
+                    type="checkbox" 
+                    name="pepperoni" /></label>
+                <label>Mushrooms
+                <input 
+                    onChange={checkChange}
+                    checked={values.mushrooms}
+                    type="checkbox" 
+                    name="mushrooms" /></label>
+                <label>Bacon
+                <input 
+                    onChange={checkChange}
+                    checked={values.bacon}
+                    type="checkbox" 
+                    name="bacon" /></label>
+                <label>Pineapple
+                <input 
+                    onChange={checkChange}
+                    checked={values.pineapple}
+                    type="checkbox" 
+                    name="pineapple" /></label>
+                <label>Olives
+                <input 
+                    onChange={checkChange}
+                    checked={values.olives}
+                    type="checkbox" 
+                    name="olives" /></label></div>
+                <label>Special Instructions:
+                <input 
+                    onChange={inputChange}
+                    value={values.instructions}
+                    type='text' 
+                    name='instructions' /></label>
+                <button onClick={onSubmit} disabled={disabled}>Submit</button>
             </form>
         </div>
     )
